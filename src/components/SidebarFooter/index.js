15762384033f4f9c8d319f0b4b2b7f175acf8ba3
Elena Spivak <spivak.e.v@gmail.com>
2020-10-17 23:46:09 +0300

    feat: add styles for Sidebar component

---
+++ src/components/SidebarFooter/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const SidebarFooter = () => (
+  <aside className="SidebarFooter">
+    Футер сайдбара
+  </aside>
+)
+
+export default SidebarFooter;
