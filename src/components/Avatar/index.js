1ac7b0b4e8a2032313ddb94936c8492b0ef81240
Elena Spivak <spivak.e.v@gmail.com>
2020-10-18 22:51:01 +0300

    feat: add size property for the Avatar component

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './style.css';
 
 const Avatar = (props) => (
-  <img className="avatar" src={props.src} alt="Аватар" width="45" height="45" />
+  <img className="avatar" src={props.src} alt="Аватар" width={props.size} height={props.size} />
 )
 
 export default Avatar;
