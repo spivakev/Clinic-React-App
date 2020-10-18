a68092befe269cde006d4c0b1eb9c81fed3ac7ba
Elena Spivak <spivak.e.v@gmail.com>
2020-10-18 16:38:36 +0300

    feat: add menu-items into menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import MenuItem from '../MenuItem'
+
 
 const Menu = () => (
-  <nav className="Menu">
-
-  </nav>
+  <ul className="menu">
+    <MenuItem iconKey="heart" text="Профиль" />
+    <MenuItem iconKey="doctors" text="Врачи и клиники" />
+    <MenuItem iconKey="messages" text="Сообщения" />
+    <MenuItem iconKey="test" text="Тестирование" />
+    <MenuItem iconKey="book" text="Полезно знать" />
+  </ul>
 )
 
 export default Menu;
