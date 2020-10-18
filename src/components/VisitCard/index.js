c1a84d7a0e7c0e15f38d50ae230cff844d9770e8
Elena Spivak <spivak.e.v@gmail.com>
2020-10-18 22:55:05 +0300

    feat: create  component for the scheduled visits - VisitCard

---
+++ src/components/VisitCard/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './style.css';
+import Avatar from '../Avatar'
+import Button from '../Button'
+
+
+const VisitCard = (props) => {
+  let visit = props.visitInfo;
+  let photoSrc = props.photoSrc;
+
+  return (
+    <div class="visit-card">
+      <div className="visit-card__wrapper">
+        <h3 className="visit-card__date">{visit.date}</h3>
+        <div className="visit-card__clinic">{visit.clinic}</div>
+        <div className="doctor visit-card__doctor">
+          <div className="doctor__avatar">
+            <Avatar src={photoSrc} size="60" />
+          </div>
+          <div className="doctor__info">
+            <div className="doctor__name">{visit.doctor}</div>
+            <div className="doctor__department">{visit.department}</div>
+          </div>
+        </div>
+      </div>
+      <div className="visit-card__btn">
+        <Button text="Отменить" />
+      </div>
+
+    </div>
+  )
+}
+
+export default VisitCard;
