7eabd4f56df73f000cdc029cccbe044026b180b7
Elena Spivak <spivak.e.v@gmail.com>
2020-10-18 19:11:00 +0300

    feat: add styles for sidebar footer

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './style.css';
-import Menu from '../../components/Menu'
-import SidebarFooter from '../../components/SidebarFooter'
+import Menu from '../../components/Menu';
+import MenuItem from '../MenuItem';
+import developer_logo from '../../img/appvelox_logo.png'
 
 const Sidebar = () => (
   <aside className="sidebar">
-    <div className="sidebar__logo">Логотип</div>
-    <Menu />
-    <SidebarFooter />
+    <div className="sidebar__content">
+      <div className="sidebar__logo">Логотип</div>
+      <Menu />
+      <Button text="Подать заявку"/>
+    </div>
+    <div className="sidebar__footer">
+      <MenuItem iconKey="info" text="Помощь" />
+      <img className="sidebar__developer-logo" src={developer_logo} width="114" height="20" alt="Appvelox" />
+    </div>
   </aside>
 )
 
