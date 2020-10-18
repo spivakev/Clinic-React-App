8e60277e2c17aa16069272dff8d14f90b8cfe1e3
Elena Spivak <spivak.e.v@gmail.com>
2020-10-18 23:02:49 +0300

    feat: add markup and styles for the section of scheduled visits

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import Sidebar from '../../components/Sidebar'
 import Header from '../../components/Header'
-import Main from '../../components/Main'
+import VisitCard from '../VisitCard'
+import malushko from '../../img/doctor_malushko.png'
+import harkov from '../../img/doctor_harkov.png'
 import './style.css';
 
 
-const Profile = () => (
-  <div className="profile profile__wrapper">
-    <Sidebar />
-    <div className="profile__content profile__wrapper">
-      <Header title="Мой профиль" />
-      <Main />
-    </div>
+const Profile = () => {
+  let visits = [
+    {
+      "date": "Понедельник 15.06.20 | 15:30",
+      "clinic": 'СПБ ГБУЗ "Городская поликлиника №25", пр. Солидарности, д. 1, к. 1, лит. А',
+      "doctor": 'Малушко Т. Н.',
+      "department": 'Хирургия'
+    }];
 
-  </div>
-)
+  return (
+    <div className="profile profile__wrapper">
+      <Sidebar />
+      <div className="profile__content profile__wrapper">
+        <Header title="Мой профиль" />
+
+        <div className="main">
+          <div className="main__container">
+            <section className="section scheduled-visits">
+              <div className="section__title">Записи на прием</div>
+              <VisitCard visitInfo={visits[0]} photoSrc={malushko} />
+            </section>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
 
 export default Profile;
