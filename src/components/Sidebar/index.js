076e64560ad4a988ae0f32f4ddb3f5080156fb16
Elena Spivak <spivak.e.v@gmail.com>
2020-10-19 21:32:14 +0300

    fix: height of the sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -7,18 +7,21 @@ import developer_logo from '../../img/appvelox_logo.png'
 
 const Sidebar = () => (
   <aside className="sidebar">
-    <div className="sidebar__content">
-      <div className="sidebar__logo">Логотип</div>
-      <Menu />
-      <div className="sidebar__btn">
-        <Button text="Подать заявку" />
+    <div className="sidebar__wrapper">
+      <div className="sidebar__content">
+        <div className="sidebar__logo">Логотип</div>
+        <Menu />
+        <div className="sidebar__btn">
+          <Button text="Подать заявку" />
+        </div>
+      </div>
+      <div className="sidebar__footer">
+        <MenuItem iconKey="info" text="Помощь" />
+        <img className="sidebar__developer-logo" src={developer_logo} width="114" height="20" alt="Appvelox" />
       </div>
     </div>
-    <div className="sidebar__footer">
-      <MenuItem iconKey="info" text="Помощь" />
-      <img className="sidebar__developer-logo" src={developer_logo} width="114" height="20" alt="Appvelox" />
-    </div>
   </aside>
+
 )
 
 export default Sidebar;
