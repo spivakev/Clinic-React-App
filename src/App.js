b5c6bdd99fc6db2dd982df3d7361575494a13128
Elena Spivak <spivak.e.v@gmail.com>
2020-10-20 17:00:16 +0300

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Route, BrowserRouter as Router} from "react-router-dom";
 import './App.css';
 import Profile from './screens/Profile'
+import Appointments from './screens/Appointments'
 require('typeface-rubik')
 
 function App() {
   return (
-    <div className="app">
-      <Profile />
-    </div>
+    <Router>
+      <div className="app">
+        <Route path="/" exact component={Profile} />
+        <Route path="/appointments" component={Appointments} />
+      </div>
+    </Router>
   );
 }
 
