aab4a6f24954ae6ddd77f91b37c0c6e467d51e19
Elena Spivak <spivak.e.v@gmail.com>
2020-10-21 18:14:32 +0300

    feat: make the menu item active all the time

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -4,7 +4,7 @@ import MenuItem from '../MenuItem'
 
 const Menu = () => (
   <nav className="menu">
-    <MenuItem iconKey="heart" text="Профиль" />
+    <MenuItem isActive={true} iconKey="heart" text="Профиль" />
     <MenuItem iconKey="doctors" text="Врачи и клиники" />
     <MenuItem iconKey="messages" text="Сообщения" />
     <MenuItem iconKey="test" text="Тестирование" />
