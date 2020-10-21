01b396e126a78744f02455ec82c9c9a6be777cef
Elena Spivak <spivak.e.v@gmail.com>
2020-10-21 18:11:24 +0300

    refactor: rename common styles classes

--- src/screens/Appointments/index.js
+++ src/screens/Appointments/index.js
@@ -5,30 +5,34 @@ import NavIcon from '../../components/NavIcon'
 import './style.css';
 import visits from '../../visits.json'
 import VisitCard from '../../components/VisitCard';
+import Calendar from '../../components/Calendar';
 
 const Appointments = () => {
+ 
   return (
-    <div className="profile profile__wrapper">
+    <div className="appointments screen screen__wrapper">
       <Sidebar />
-      <div className="profile__content profile__wrapper">
+      <div className="screen__content screen__wrapper">
         <Header title="Мой профиль" />
 
-        <div className="main">
-          <div className="main__container">
+        <div className="appointments__main">
+          <div className="appointments__back">
+            <NavIcon to="/" iconKey="back" />
+            <div className="section-title appointments__section-title ">Мои записи</div>
+          </div>
+          <div className="appointments__container">
             <section className="section my-appointments">
-              <div className="section__title-wrapper">
-                <NavIcon to="/" iconKey="back" />
-                <div className="section__title">Мои записи</div>
-              </div>
               <div className="my-appointments__wrapper">
                 <div className="my-appointments__content">
                   {visits.map(visitInfo => <VisitCard visitInfo={visitInfo} />
                   )}
                 </div>
               </div>
-
-
-
+            </section>
+            <section>
+              <div className="appointments__calendar">
+                <Calendar />
+              </div>
             </section>
           </div>
         </div>
