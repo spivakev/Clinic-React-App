d44f104b9e097d5463b57a67806587ec1fa019fa
Elena Spivak <spivak.e.v@gmail.com>
2020-10-30 16:00:20 +0300

    swipe rotes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router} from "react-router-dom";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import './App.css';
 import Profile from './screens/Profile'
 import Appointments from './screens/Appointments'
@@ -9,8 +9,8 @@ function App() {
   return (
     <Router>
       <div className="app">
-        <Route path="/" exact component={Profile} />
         <Route path="/appointments" component={Appointments} />
+        <Route path="/" exact component={Profile} />
       </div>
     </Router>
   );
