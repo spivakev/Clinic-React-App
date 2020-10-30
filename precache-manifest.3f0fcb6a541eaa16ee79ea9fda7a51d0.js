0456446063a002b04df3debb035f73a237ed07fc
Elena Spivak <spivak.e.v@gmail.com>
2020-10-30 15:33:56 +0300

    Updates

---
+++ precache-manifest.3f0fcb6a541eaa16ee79ea9fda7a51d0.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0918ac41865895c4b3c2eb4c41e3f5",
+    "url": "/Clinic-React-App/index.html"
+  },
+  {
+    "revision": "8a26f5da932370923fc1",
+    "url": "/Clinic-React-App/static/css/2.1036e922.chunk.css"
+  },
+  {
+    "revision": "10b8922ebe4c40931de6",
+    "url": "/Clinic-React-App/static/css/main.37e1783a.chunk.css"
+  },
+  {
+    "revision": "8a26f5da932370923fc1",
+    "url": "/Clinic-React-App/static/js/2.d0491be5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Clinic-React-App/static/js/2.d0491be5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10b8922ebe4c40931de6",
+    "url": "/Clinic-React-App/static/js/main.1c3a59ac.chunk.js"
+  },
+  {
+    "revision": "c3f11f2c399e3c902df3",
+    "url": "/Clinic-React-App/static/js/runtime-main.b32eba7d.js"
+  },
+  {
+    "revision": "7843bd8573fec1dd5788f21dcae17180",
+    "url": "/Clinic-React-App/static/media/doctor_harkov.7843bd85.png"
+  },
+  {
+    "revision": "7ca73bdf2575aafe1ab443dfe590bd6f",
+    "url": "/Clinic-React-App/static/media/doctor_malushko.7ca73bdf.png"
+  },
+  {
+    "revision": "a903783d5fd65d46180254139749fd7a",
+    "url": "/Clinic-React-App/static/media/patient.a903783d.png"
+  },
+  {
+    "revision": "130b18e84542cf7bad69085d1fc68cb8",
+    "url": "/Clinic-React-App/static/media/rubik-latin-300.130b18e8.woff2"
+  },
+  {
+    "revision": "f22c51cad998da34a005f264297a1b06",
+    "url": "/Clinic-React-App/static/media/rubik-latin-300.f22c51ca.woff"
+  },
+  {
+    "revision": "bc9d245d7bd36f3118b013d7977bb037",
+    "url": "/Clinic-React-App/static/media/rubik-latin-300italic.bc9d245d.woff2"
+  },
+  {
+    "revision": "e7332afb370f35b7bb186696b2a9a85d",
+    "url": "/Clinic-React-App/static/media/rubik-latin-300italic.e7332afb.woff"
+  },
+  {
+    "revision": "ae59d6bdaf6d57315cfd2633349ffd64",
+    "url": "/Clinic-React-App/static/media/rubik-latin-400.ae59d6bd.woff2"
+  },
+  {
+    "revision": "c686373664d870fc7ac36691561f68b6",
+    "url": "/Clinic-React-App/static/media/rubik-latin-400.c6863736.woff"
+  },
+  {
+    "revision": "230a8e72aa83c91a5fec8e42c636d342",
+    "url": "/Clinic-React-App/static/media/rubik-latin-400italic.230a8e72.woff2"
+  },
+  {
+    "revision": "23b64e24e6c136186af7505175fc0584",
+    "url": "/Clinic-React-App/static/media/rubik-latin-400italic.23b64e24.woff"
+  },
+  {
+    "revision": "4e59691c67f4b311a984039c4eba691f",
+    "url": "/Clinic-React-App/static/media/rubik-latin-500.4e59691c.woff"
+  },
+  {
+    "revision": "f5b75c822c9be4f08fdc59599e24eddd",
+    "url": "/Clinic-React-App/static/media/rubik-latin-500.f5b75c82.woff2"
+  },
+  {
+    "revision": "5bf4a9438d9ddae7002bd3dd1ab69068",
+    "url": "/Clinic-React-App/static/media/rubik-latin-500italic.5bf4a943.woff"
+  },
+  {
+    "revision": "c04f584fadeb15b5c6409906cef146c9",
+    "url": "/Clinic-React-App/static/media/rubik-latin-500italic.c04f584f.woff2"
+  },
+  {
+    "revision": "8b116f1484cb41affc654e7223b6aee8",
+    "url": "/Clinic-React-App/static/media/rubik-latin-600.8b116f14.woff2"
+  },
+  {
+    "revision": "9205b4cf980f5b959bd113d9faf6470a",
+    "url": "/Clinic-React-App/static/media/rubik-latin-600.9205b4cf.woff"
+  },
+  {
+    "revision": "6586cb5f710dcd1ed391cdd50688f7c4",
+    "url": "/Clinic-React-App/static/media/rubik-latin-600italic.6586cb5f.woff"
+  },
+  {
+    "revision": "e73ebb175319bb81b647e8e53eba8786",
+    "url": "/Clinic-React-App/static/media/rubik-latin-600italic.e73ebb17.woff2"
+  },
+  {
+    "revision": "60e4b57e8e09e83f99923ae56a686a1f",
+    "url": "/Clinic-React-App/static/media/rubik-latin-700.60e4b57e.woff2"
+  },
+  {
+    "revision": "b648e18b90bcf0a62f9d8af11c52750b",
+    "url": "/Clinic-React-App/static/media/rubik-latin-700.b648e18b.woff"
+  },
+  {
+    "revision": "9d546c1eaebc6eb68a4b260cd86ea0bd",
+    "url": "/Clinic-React-App/static/media/rubik-latin-700italic.9d546c1e.woff2"
+  },
+  {
+    "revision": "b0bffa2a7e17a08d4736d7ea5b32808f",
+    "url": "/Clinic-React-App/static/media/rubik-latin-700italic.b0bffa2a.woff"
+  },
+  {
+    "revision": "35f5ebb9926bd9eedf9f246ce2f84eea",
+    "url": "/Clinic-React-App/static/media/rubik-latin-800.35f5ebb9.woff2"
+  },
+  {
+    "revision": "a37e9453f2655627c9176ee6529726cc",
+    "url": "/Clinic-React-App/static/media/rubik-latin-800.a37e9453.woff"
+  },
+  {
+    "revision": "500684778a55bd29efdfdbf5f839aa2d",
+    "url": "/Clinic-React-App/static/media/rubik-latin-800italic.50068477.woff"
+  },
+  {
+    "revision": "bf70586c318124418eee88c34606b2b7",
+    "url": "/Clinic-React-App/static/media/rubik-latin-800italic.bf70586c.woff2"
+  },
+  {
+    "revision": "0c66ee946257e9e723941c89df3a0110",
+    "url": "/Clinic-React-App/static/media/rubik-latin-900.0c66ee94.woff"
+  },
+  {
+    "revision": "73b53b24747074746d6d28d1193d6fb0",
+    "url": "/Clinic-React-App/static/media/rubik-latin-900.73b53b24.woff2"
+  },
+  {
+    "revision": "42d602678906c106eaae3e850ffcc013",
+    "url": "/Clinic-React-App/static/media/rubik-latin-900italic.42d60267.woff"
+  },
+  {
+    "revision": "7f1410c28c8f2399e49cdc39af5e2e81",
+    "url": "/Clinic-React-App/static/media/rubik-latin-900italic.7f1410c2.woff2"
+  }
+]);
